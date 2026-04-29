9368204a8e79282e91683e32b5d2bfd254773b94
lpinelis-afk <lpinelis@student.smithschool.org>
2026-04-29 10:52:52 -0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import pandas as pd
+category=input("Book/Movie/Show> ")
+creator=input("> ")
+movies = pd.read_csv('./CSV/movies.csv')
+books = pd.read_csv('./CSV/books.csv')
+
+
+# Filter the dataframe where the 'cast' column contains the actor's name
+movies_with_actor = movies[movies['cast'].str.contains(creator, case=False, na=False)]
+books_with_actor = books[books['Book-Author'].str.contains(creator, case=False, na=False)]
+
+
+# Print the titles of those movies
+if category == "Book":
+    print(books_with_actor['Book-Title'])
+else:
+    print(movies_with_actor['title'])
