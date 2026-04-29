7c32f9b7ebe482579a7d32d1f1486e1f38844730
lpinelis-afk <lpinelis@student.smithschool.org>
2026-04-29 10:59:54 -0400

    test commit

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ movies = pd.read_csv('./CSV/movies.csv')
 books = pd.read_csv('./CSV/books.csv')
 
 a ="test"
+b = "test"
 # Filter the dataframe where the 'cast' column contains the actor's name
 movies_with_actor = movies[movies['cast'].str.contains(creator, case=False, na=False)]
 books_with_actor = books[books['Book-Author'].str.contains(creator, case=False, na=False)]
